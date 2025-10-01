3a42f93e26e684179ae8b33584bcb69a09187c55
B0x Token or B ZERO X Token <bzeroxtoken@gmail.com>
2025-10-01 00:56:56 -0500

    Update price_monitor_github.py

--- price_monitor_github.py
+++ price_monitor_github.py
@@ -245,8 +245,7 @@ def getSlot0(block):
     if price == 0 or price < 1e-16:
         print(f"WARNING: BWORK/WETH price is zero or too small at block {block}")
         actual_price = 0
-        return actual_price;
-    else
+        return actual_price
     
     # WETH/USD POOL
     pool_slot = '0xe570f6e770bf85faa3d1dbee2fa168b56036a048a7939edbcd02d7ebddf3f948'
